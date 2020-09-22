6a4f49af8a29d4b1c119ee80f46e3d38e72c7954
Timothy <timothy120500@gmail.com>
2020-09-22 18:00:47 +0000

    Added  the rest of the cases

--- main.c
+++ main.c
@@ -16,9 +16,27 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
+  if (s < d) {
+      printf("ERROR\n");
+      // End program, with error code
+      return 1;
+  }
+
   if(s < 120) {
     if(d < 80) {
       printf("Normal\n");
     }
+  } else if (s < 130) {
+    if (d < 80) {
+        printf("Elavated\n");
+    }
+  } else if (s < 140 || (d >= 80 && d < 90 )) {
+    printf("Stage 1 Hypertension\n");
+  } else if (s > 140 || d > 90) {
+    printf("Stage 2 Hypertension\n");
+  } else if (s > 180 || d > 120) {
+    printf("Hypertensive Crysis\n"); 
   }
+
+  return 0;
 }
