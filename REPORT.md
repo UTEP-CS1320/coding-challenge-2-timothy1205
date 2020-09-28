479aff420f42ba0c6a2686a65e70540245916444
Timothy <timothy120500@gmail.com>
2020-09-28 18:38:44 +0000

    Reordered cases, final report.

--- REPORT.md
+++ REPORT.md
@@ -3,4 +3,6 @@
 
 ### CODING CHALLENGE #2: REPORT
 
-(Write your report here)
+I used a total of 6 if statements, 5 of which are used in an if-else-if chain. The initial if statement is a check to see if the Systolic Pressure is less than the Diastolic Pressure as if this is ever true, we should simply print "ERROR" and end the program. The next 5 if statements are used for the 5 categories. I opted to order them backwards as I felt it made more sense. The fact that the last 3 statements are OR statements also made the logic easier for me to work with, as I simply checked the pressures in descending order so I wouldn't need to use unnecessarily conditions. Each condition compares the systolic pressure and diastolic pressure to correspond to the information included in the README. (For the Hypertensive Crysis category, I left it as an OR statement since it covers the possible AND case.)
+
+I believe one if statement for each possible case is the absolute minimum possible, so assuming I didn't miss an edge while testing my code, I believe this is the smallest amount of if statements required for this to function.
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 *   CODING CHALLENGE #2
 *   Filename: main.c
 *   This file measures a person's blood pressure readings and reports their category.
-*   Last modified by: Angel F. Garcia (9/19/2020)
+*   Last modified by: Timothy Gonzales (9/28/2020)
 *   
 *   Check the // TODO comments to see what is missing and needs to be completed.
 */
@@ -15,28 +15,23 @@ int main(void) {
   // Read Systolic and Diastolic pressure
   scanf("%d %d", &s, &d);
 
-  // TODO: Complete the rest of the categories
   if (s < d) {
       printf("ERROR\n");
       // End program, with error code
       return 1;
   }
 
-  if(s < 120) {
-    if(d < 80) {
-      printf("Normal\n");
-    }
-  } else if (s < 130) {
-    if (d < 80) {
-        printf("Elavated\n");
-    }
-  } else if (s < 140 || (d >= 80 && d < 90 )) {
-    printf("Stage 1 Hypertension\n");
-  } else if (s > 140 || d > 90) {
-    printf("Stage 2 Hypertension\n");
-  } else if (s > 180 || d > 120) {
+  if (s >= 180 || d > 120) {
     printf("Hypertensive Crysis\n"); 
-  }
+  } else if (s >= 140 || d > 90) {
+    printf("Stage 2 Hypertension\n");
+  } else if (s >= 130 || (d >= 80 && d < 90 )) {
+    printf("Stage 1 Hypertension\n");
+  } else if (s >= 120 && d < 80) {
+        printf("Elavated\n");
+  } else if (s < 120 && d < 80) {
+      printf("Normal\n");
+  } 
 
   return 0;
 }
